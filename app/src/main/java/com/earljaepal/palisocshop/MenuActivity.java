8f9090cfaf0bdf4876d6943821af0aaf56050372
origin <earlj_pal@hotmail.ca>
2019-09-19 19:12:49 -0400

    Commit - can navigate through all layouts

--- app/src/main/java/com/earljaepal/palisocshop/MenuActivity.java
+++ app/src/main/java/com/earljaepal/palisocshop/MenuActivity.java
@@ -1,13 +1,25 @@
 package com.earljaepal.palisocshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MenuActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void launchCheckoutMenu(View view) {
+        Intent checkout = new Intent(this, CheckoutActivity.class);
+        startActivity(checkout);
+        Log.d(LOG_TAG, "Proceed to checkout");
     }
 }
